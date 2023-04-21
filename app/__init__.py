3be9e8dbe4aacbda1252a1216809ccad6b5ff59c
prince camb <prince.camb100@gmail.com>
2023-04-21 13:36:27 +0530

    “message”

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,10 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="######################################",
-        database="######",
-        user="#######",
-        password="################")
+        host="dpg-ch12s0r3cv203bug32q0-a.oregon-postgres.render.com",
+        database="todo_xnjc",
+        user="todo_xnjc_user",
+        password="wdu6dsHha8v06L8JYJX0228HYYC3Mvs7")
 # To prevent from using a blueprint, we use a cyclic import
 # This also means that we need to place this import here
 # pylint: disable=cyclic-import, wrong-import-position
